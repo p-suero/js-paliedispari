b76d84d9b9fe4364664a23810e474946b931ee84
Paolo <paolo.suero@gmail.com>
2020-04-25 15:00:25 +0200

    Terminato primo esercizio

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,1 +1,28 @@
 // scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
+
+//creo una funzione per capire se una parola è palindroma
+function palindroma(parola)  {
+    //trasformo la parola in un array (ogni lettera un valore), inverto i valori dell'array e lo ritrasformo in un'unica stringa
+    var parola_reverse = parola.split("").reverse().join("");
+    //esamino la condizione e restituisco un risultato per la funzione
+    if (parola == parola_reverse) {
+        return(true);
+    } else {
+        return(false);
+    }
+}
+
+//chiedo all'utente di inserire una parola
+var parola_utente = prompt("Inserisci una parola per verificare se è palindroma").toUpperCase();
+
+//stampare il risultato se la funzione è vera oppure no
+if (isNaN(parola_utente)) {
+    if (palindroma(parola_utente) == true) {
+        console.log("La parola " + parola_utente + " è palindroma");
+    } else {
+        console.log("La parola " + parola_utente + " non è palindroma");
+    }
+} else {
+    alert("Hai inserito un valore non valido");
+    location.reload();
+}
