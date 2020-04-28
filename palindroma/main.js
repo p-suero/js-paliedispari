d836a6f80971dea29181f50fa2a2874a5ce67a96
Paolo <paolo.suero@gmail.com>
2020-04-28 14:50:12 +0200

    Perfezionati gli esercizi

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,14 +1,19 @@
 // scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
 
-//creo una funzione per capire se una parola è palindroma
-function palindroma(parola)  {
-    //trasformo la parola in un array (ogni lettera un valore), inverto i valori dell'array e lo ritrasformo in un'unica stringa
+//creo una funzione che mi restituisce la parola al contrario
+function reverse(parola)  {
+    //trasformo la parola in un array (ogni lettera un valore), inverto i valori dell'array e lo ritrasformo in un'unica stringa per ottenrla invertita
     var parola_reverse = parola.split("").reverse().join("");
     //esamino la condizione e restituisco un risultato per la funzione
-    if (parola == parola_reverse) {
-        return(true);
+    return parola_reverse
+}
+
+// creo una funzione che restituisce true se una parola è palindroma e viceversa
+function is_palindroma(parola) {
+    if (reverse(parola) == parola) {
+        return(true)
     } else {
-        return(false);
+        return(false)
     }
 }
 
@@ -17,7 +22,7 @@ var parola_utente = prompt("Inserisci una parola per verificare se è palindroma
 
 //Dopo aver validato i dati, stampo il risultato in base a se la funzione è vera oppure no
 if (isNaN(parola_utente) && parola_utente.length > 1) {
-    if (palindroma(parola_utente) == true) {
+    if (is_palindroma(parola_utente) == true) {
         console.log("La parola " + parola_utente + " è palindroma");
     } else {
         console.log("La parola " + parola_utente + " non è palindroma");
--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -2,43 +2,28 @@
 
 //chiedo all'utente di scegliere un numero da 1 a 5
 var numero_utente = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log("Hai inserito: " + numero_utente);
+
 //valido l'input del prompt precedente
 if (isNaN(numero_utente) || numero_utente < 1 || numero_utente > 5) {
-    alert("Hai inserito un numero non valido. Riprova");
+    alert("Hai inserito un valore non valido. Riprova");
     location.reload();
 } else {
     //chiedo all'utente di scegliere tra pari o dispari. (Uso il lower case per rendere valido il carattere grassetto)
     var pari_dispari_utente = prompt("Scegli Pari o Dispari?").toLowerCase();
+    console.log("Hai scelto: " + pari_dispari_utente.toUpperCase());
+
     //valido l'input precedente
     if (pari_dispari_utente != "pari" && pari_dispari_utente != "dispari") {
        alert("Hai inserito un valore diverso da pari o dispari");
        location.reload();
     } else {
-        //creo una funzione che generi un numero random
-        function generaRandom(min, max) {
-            var numero = Math.floor(Math.random() * (max - min + 1)) + min;
-            return numero;
-        }
-
         //salvo il numero dato dalla funzione "generaRandom" in una variabile
         var numero_generato = generaRandom(1, 5);
-
-        //creo una funzione che calcoli se un numero è pari o meno
-        function is_even(numero) {
-            if (numero % 2 == 0) {
-                return(true);
-            } else {
-                return(false);
-            }
-        }
+        console.log("Il PC ha scelto = " + numero_generato);
 
         //sommo il numero dato dall'utente ed il numero generato dal pc (attravero la funzione)
         var somma = numero_utente + numero_generato;
-
-        //stampo le informazioni
-        console.log("Hai scelto: " + pari_dispari_utente.toUpperCase());
-        console.log("Hai inserito: " + numero_utente);
-        console.log("Il PC ha scelto = " + numero_generato);
         console.log("SOMMA = " + somma);
 
         //stampo il risultato del mini-gioco quando si verifica una delle seguneti condizioni
@@ -51,5 +36,20 @@ if (isNaN(numero_utente) || numero_utente < 1 || numero_utente > 5) {
         } else {
             console.log("Hai perso. La somma dei numeri " + numero_utente + " + " + numero_generato +  " è dispari");
         }
+    }
+}
+
+//creo una funzione che generi un numero random
+function generaRandom(min, max) {
+    var numero = Math.floor(Math.random() * (max - min + 1)) + min;
+    return numero;
+}
+
+//creo una funzione che calcoli se un numero è pari o meno
+function is_even(numero) {
+    if (numero % 2 == 0) {
+        return(true);
+    } else {
+        return(false);
     }
 }
